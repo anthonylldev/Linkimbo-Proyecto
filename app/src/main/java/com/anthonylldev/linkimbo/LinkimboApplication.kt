e3675e5c6ae505ec310c6f58728087189855bdcd
Anthony Fabian Leon Lucero <anthonyfabian22ll@gmail.com>
2022-04-18 17:25:05 +0200

    Implement Timber in Application

--- app/src/main/java/com/anthonylldev/linkimbo/LinkimboApplication.kt
+++ app/src/main/java/com/anthonylldev/linkimbo/LinkimboApplication.kt
@@ -2,8 +2,12 @@ package com.anthonylldev.linkimbo
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class LinkimboApplication : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 }
