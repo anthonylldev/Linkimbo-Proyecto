e07367819187b28cb5a3327086e6e94fed681cde
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-06-14 16:08:20 +0200

    Add post rest controller and post service

---
+++ app/src/main/java/com/anthonylldev/linkimbo/post/application/service/PostService.kt
@@ -0,0 +1,10 @@
+package com.anthonylldev.linkimbo.post.application.service
+
+import com.anthonylldev.linkimbo.post.domain.model.Post
+
+interface PostService {
+
+    suspend fun insertPost(request: Post)
+
+    suspend fun getAllPostSortByTimestamp(): List<Post>
+}
